e3fae66c9169b77d6c18e3a571c3517d80be2523
Berin <berin@web-vision.de>
2020-11-02 01:49:35 +0100

    Add system configuration and module's helper

---
+++ Block/Adminhtml/Form/Field/Attributes.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @category  BKozlic
+ * @package   BKozlic\ConfigurableOptions
+ * @author    Berin Kozlic - berin.kozlic@gmail.com
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace BKozlic\ConfigurableOptions\Block\Adminhtml\Form\Field;
+
+use Magento\Config\Block\System\Config\Form\Field\FieldArray\AbstractFieldArray;
+
+/**
+ * Class Attributes
+ * @package Bunited\SimpleOptions\Block\Adminhtml\Form\Field
+ */
+class Attributes extends AbstractFieldArray
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function _prepareToRender()
+    {
+        $this->addColumn('identity', ['label' => __('Element Class or ID'), 'class' => 'required-entry']);
+        $this->addColumn('simple_product_attribute', ['label' => __('Product Attribute'), 'class' => 'required-entry']);
+        $this->_addAfter = false;
+        $this->_addButtonLabel = __('Add');
+    }
+}
---
+++ Helper/Data.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @category  BKozlic
+ * @package   BKozlic\ConfigurableOptions
+ * @author    Berin Kozlic - berin.kozlic@gmail.com
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+namespace BKozlic\ConfigurableOptions\Helper;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Configurable Options Data Helper
+ */
+class Data extends AbstractHelper
+{
+    const XML_PATH_ENABLED = 'configurable_options/configurable_general/enabled';
+    const XML_PATH_GALLERY = 'configurable_options/configurable_frontend/gallery_switch';
+    const XML_PATH_PRESELECTED_ENABLED = 'configurable_options/configurable_frontend/preselected_options';
+    const XML_PATH_SIMPLE_UPDATE = 'configurable_options/configurable_frontend/simple_details';
+    const XML_PATH_ATTRIBUTES = 'configurable_options/product_attributes/simple_updates';
+
+    /**
+     * Scope config
+     *
+     * @var ScopeConfigInterface
+     */
+    protected $config;
+
+    /**
+     * @param ScopeConfigInterface $config
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        ScopeConfigInterface $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
+     * Check if extension is enabled
+     *
+     * @return boolean
+     */
+    public function isEnabled()
+    {
+        return $this->config->getValue(self::XML_PATH_ENABLED, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Check if preselect option is enabled
+     *
+     * @return boolean
+     */
+    public function isPreselectedEnabled()
+    {
+        return $this->config->getValue(self::XML_PATH_PRESELECTED_ENABLED, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Check if update simple detail option is enabled
+     *
+     * @return boolean
+     */
+    public function isSimpleProductUpdateEnabled()
+    {
+        return $this->config->getValue(self::XML_PATH_SIMPLE_UPDATE, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Returns gallery switch strategy
+     *
+     * @return string
+     */
+    public function getGallerySwitchStrategy()
+    {
+        return $this->config->getValue(self::XML_PATH_GALLERY, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Returns attributes data
+     *
+     * @return string
+     */
+    public function getSimpleAttributes()
+    {
+        return $this->config->getValue(self::XML_PATH_ATTRIBUTES, ScopeInterface::SCOPE_STORE);
+    }
+}
